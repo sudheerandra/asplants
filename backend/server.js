e5ef21290fdb3d78a8c73ca4474c9d14d55301f5
sudheerandra <sudheerbabu.andra@gmail.com>
2025-09-16 15:44:52 +0530

    server issue

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,9 @@ connectCloudinary();
 // middlewares
 app.use(
   cors({
-    origin: [
-      process.env.CLIENT_PROD, // production frontend
-      process.env.CLIENT_URL, // local dev frontend
-    ],
+    origin: process.env.NODE_ENV === "production" 
+      ? process.env.CLIENT_PROD
+      : process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
